16f50b6637253613aeb70b715b8a5081e9cb32dd
ottovisocks <otto.visocks@gmail.com>
2021-09-10 14:27:20 +0300

    insert data + select.ts

--- sql-shopify/src/queries/select.ts
+++ sql-shopify/src/queries/select.ts
@@ -1,28 +1,43 @@
+import {
+  APPS,
+  APPS_CATEGORIES,
+  CATEGORIES,
+  REVIEWS
+} from "../shopify-table-names";
+
 export const selectCount = (table: string): string => {
-  throw new Error(`todo`);
+  return `SELECT COUNT (*) AS c FROM ${table}`;
 };
 
 export const selectRowById = (id: number, table: string): string => {
-  throw new Error(`todo`);
+  return `SELECT * FROM ${table} WHERE id = ${id}`;
 };
 
 export const selectCategoryByTitle = (title: string): string => {
-  throw new Error(`todo`);
+  return `SELECT * FROM ${CATEGORIES} WHERE title = '${title}'`;
 };
 
 export const selectAppCategoriesByAppId = (appId: number): string => {
-  throw new Error(`todo`);
+  return `
+    SELECT a.title AS app_title, category_id, c.title AS category_title 
+      FROM ${APPS_CATEGORIES} ac 
+      JOIN ${APPS} a ON a.id = ac.app_id
+      JOIN ${CATEGORIES} c ON c.id = ac.category_id
+      WHERE a.id = ${appId}`;
 };
 
 export const selectUnigueRowCount = (tableName: string, columnName: string): string => {
-  throw new Error(`todo`);
+  return `SELECT COUNT (DISTINCT ${columnName}) AS c FROM ${tableName}`;
 };
 
 export const selectReviewByAppIdAuthor = (appId: number, author: string): string => {
-  throw new Error(`todo`);
+  return `SELECT * FROM ${REVIEWS} WHERE app_id = ${appId} AND author = '${author}'`;
 };
 
 export const selectColumnFromTable = (columnName: string, tableName: string): string => {
-  throw new Error(`todo`);
+  return `SELECT ${columnName} FROM ${tableName}`;
 };
 
+export const selectAllRows = (table: string): string => {
+  return `SELECT * FROM ${table}`;
+};
