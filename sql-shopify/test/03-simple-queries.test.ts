462cd31d61a873c1b1388b6b4de7b8702c5d04a5
ottovisocks <otto.visocks@gmail.com>
2021-09-10 15:27:27 +0300

    simple queries

--- sql-shopify/test/03-simple-queries.test.ts
+++ sql-shopify/test/03-simple-queries.test.ts
@@ -1,5 +1,9 @@
 import { Database } from "../src/database";
 import { minutes } from "./utils";
+import {
+    APPS,
+    REVIEWS
+} from "../src/shopify-table-names";
 
 describe("Simple Queries", () => {
     let db: Database;
@@ -9,7 +13,7 @@ describe("Simple Queries", () => {
     }, minutes(1));
 
     it("should select app count with rating of 5 stars", async done => {
-        const query = `todo`;
+        const query = `SELECT COUNT (*) AS count FROM ${APPS} WHERE rating = 5`;
         const result = await db.selectSingleRow(query);
         expect(result).toEqual({
             count: 731
@@ -18,8 +22,14 @@ describe("Simple Queries", () => {
     }, minutes(1));
 
     it("should select top 3 develepors with most apps published", async done => {
-        const query = `todo`;
-
+        const query = `
+            SELECT developer, COUNT (*) AS count
+                FROM ${APPS}
+                GROUP BY developer
+                ORDER BY count DESC, developer ASC 
+                LIMIT 3
+            `;
+            
         const result = await db.selectMultipleRows(query);
         expect(result).toEqual([
             { count: 30, developer: "Webkul Software Pvt Ltd" },
@@ -30,7 +40,13 @@ describe("Simple Queries", () => {
     }, minutes(1));
 
     it("should select count of reviews created in year 2014, 2015 and 2016", async done => {
-        const query = `todo`;
+        const query = `
+            SELECT COUNT (*) AS review_count,
+                SUBSTR (date_created, 7, 4) AS year
+                FROM ${REVIEWS}
+                WHERE (year = '2014' OR year = '2015' OR year = '2016')
+                GROUP BY year
+            `;
         const result = await db.selectMultipleRows(query);
         expect(result).toEqual([
             { year: "2014", review_count: 6157 },
